a6181787379adc7b8b70dcc57d506c00b1835065
macbook <macbook@May-tinh-xach-tay-cua-macbook.local>
2025-11-09 12:22:54 +0700

    update all

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ task_map = {
 # Change variable here
 ALGORITHM_NAME = 'mpage'  # Could also be 'MEoH' or 'NSGA2'
 PROBLEM_NAME = "bi_cvrp" # Could also be "tsp_semo, bi_kp, bi_cvrp"
-exact_log_dir_name = "nhv_runtime/v2" # must be unique here
-api_key = os.getenv('API_KEY4') # change APIKEY1, APIKEY2, APIKEY3
+exact_log_dir_name = "nhv_runtime/v3" # must be unique here
+api_key = os.getenv('API_KEY1') # change APIKEY1, APIKEY2, APIKEY3
 
 if __name__ == '__main__':
     
